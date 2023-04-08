147bfc32fe3048a30ef6b4529ea8ca798cfb32a3
spundafr <spundafr@gmail.com>
2023-04-08 12:56:42 +0200

    inicialize repozitory

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+// Interakce Node.js s file systémem.
+import nodeNext from './nodeNext.js'
+import commonJs from './commonJs.cjs'
+
+console.log(commonJs)
+console.log(nodeNext)
---
+++ src/nodeNext.ts
@@ -0,0 +1,5 @@
+import fs from 'fs'
+
+const text = 'Toto je import z NodeNext souboru.'
+
+export default { text, fs: fs.Dir }
